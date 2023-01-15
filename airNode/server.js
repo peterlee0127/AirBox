ac7cf485855503fb3533a254a8d0b4e62217876b
peter.lee <peterlee0127@gmail.com>
2023-01-15 15:50:43 +0800

    update

--- airNode/server.js
+++ airNode/server.js
@@ -16,20 +16,20 @@ io.on('connection', function(socket){
 let file = fs.readFileSync('public/monitor.html',"utf-8");
 // response
 app.use(ctx => {
-	if(ctx.path=="/monitor"){    
+	if(ctx.path=="/monitor"){
 		ctx.type = "html";
 		ctx.body = file;
 	}else {
 		ctx.body = data;
 	}
 });
-
+let i = 1;
 function update() {
     request.get({url:'http://192.168.2.97/'}, function(err,httpResponse,body){
 	if(err){
 		return;
 	}else {
-    
+
         data = JSON.parse(body);
         if(data['pm2.5']>400){
             return;
@@ -41,17 +41,23 @@ function update() {
         if(data["SSID"]) {
             delete data["SSID"];
         }
-        data = JSON.stringify(data).replace(/\r/g,"");	
+        data = JSON.stringify(data).replace(/\r/g,"");
         if(io.engine.clientsCount>0) {
             io.emit('data', data);
         }
-	    console.log(data);
+	if(i%2==0) {
+		console.log(data);
+		i = 1;
 	}
-    
+	else {
+		i = i + 1;
+	}
+
+	}
     });
 }
 
-setInterval(update,10000);
+setInterval(update,5000);
 update();
 
 server.listen(3333);
